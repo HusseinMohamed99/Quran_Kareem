33bd9459590297852ffd2f296336078333fed544
DESKTOP-O90U05F\pc <value9660@gmail.com>
2024-08-15 10:49:29 +0300

    Add Arabic Transliteration and Refactor Code for Clarity and Consistency
    - Added 'suwar' to the list of keywords in .vscode/settings.json for Arabic transliteration support.
    - Refactored import statements in moshaf_screen.dart to maintain consistency and improve readability.
    - Removed unnecessary GestureDetector and urlLauncher call in reciters_details_widget.dart, simplifying the widget for better maintainability.
    - Integrated shared_preferences_foundation plugin in GeneratedPluginRegistrant.swift for macOS application state management.
    - Updated pubspec.lock and pubspec.yaml to include device_preview and shared_preferences packages for enhanced UI testing and persistent storage capabilities.
    
    These changes aim to improve the app's transliteration support, code readability, and functionality, ensuring a smoother development experience and better end-user experience.

--- lib/shared/widget/reciters_details_widget.dart
+++ lib/shared/widget/reciters_details_widget.dart
@@ -3,7 +3,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:moshaf_app/image_assets.dart';
 import 'package:moshaf_app/model/reciters_model.dart';
 import 'package:moshaf_app/shared/Colors/color_manager.dart';
-import 'package:moshaf_app/shared/components/launcher.dart';
 
 class RecitersDetailsWidget extends StatelessWidget {
   const RecitersDetailsWidget({
@@ -18,50 +17,43 @@ class RecitersDetailsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        urlLauncher(
-          Uri.parse(recitersModel.reciters![number].moshaf![index].server!),
-        );
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-        margin: const EdgeInsets.symmetric(horizontal: 20),
-        decoration: BoxDecoration(
-          color: const Color.fromRGBO(17, 32, 149, 100),
-          borderRadius: const BorderRadius.all(
-            Radius.circular(10),
-          ),
-          border: Border.all(
-            color: ColorsManager.kBlueColor,
-          ),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(17, 32, 149, 100),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(10),
         ),
-        child: Row(
-          children: [
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                SvgPicture.asset(Assets.imagesIconMuslim),
-                Text(
-                  recitersModel.reciters![number].moshaf![index].id.toString(),
-                  style: const TextStyle(
-                    color: ColorsManager.kWhiteColor,
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: Text(
-                recitersModel.reciters![number].moshaf![index].name!,
-                textAlign: TextAlign.center,
+        border: Border.all(
+          color: ColorsManager.kBlueColor,
+        ),
+      ),
+      child: Row(
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              SvgPicture.asset(Assets.imagesIconMuslim),
+              Text(
+                recitersModel.reciters![number].moshaf![index].id.toString(),
                 style: const TextStyle(
                   color: ColorsManager.kWhiteColor,
-                  fontSize: 20,
                 ),
               ),
+            ],
+          ),
+          Expanded(
+            child: Text(
+              recitersModel.reciters![number].moshaf![index].name!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: ColorsManager.kWhiteColor,
+                fontSize: 20,
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import audioplayers_darwin
 import connectivity_plus
 import path_provider_foundation
 import share_plus
+import shared_preferences_foundation
 import url_launcher_macos
 import video_player_avfoundation
 
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
