fa2bd736aec3ebba266f794e30c8a4319cbbc1f9
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-10-19 15:54:07 +0300

    Add Flutter ScreenUtil, In-App Review, and Upgrader Functionality
    This commit introduces several enhancements to the application:
    
    1. **Flutter ScreenUtil Integration**: Added `flutter_screenutil` to ensure responsive design across different screen sizes. This includes initializing ScreenUtil and updating the MaterialApp to use ScreenUtilInit for better UI consistency.
    
    2. **In-App Review Feature**: Implemented the `in_app_review` package to allow users to rate the app directly from within the application. The `requestReview` function is called in release mode to prompt users for a review.
    
    3. **Upgrader Functionality**: Integrated the `upgrader` package to handle app updates. This includes wrapping the main application widget with `UpgradeWrapper` to display update prompts and clearing saved settings in debug mode.
    
    4. **Firebase Crashlytics Configuration**: Enhanced Firebase Crashlytics setup to only record errors in release mode, ensuring that only "fatal" errors are reported.
    
    5. **Onboarding Screen Update**: Wrapped the onboarding screen with `UpgradeWrapper` to ensure that users are prompted to update the app if a new version is available.
    
    6. **Dependency Updates**: Added new dependencies (`flutter_screenutil`, `in_app_review`, `package_info_plus`, `upgrader`) and updated existing ones (`url_launcher`). The `pubspec.lock` file has been updated to reflect these changes.
    
    These changes aim to improve the user experience by ensuring the app is responsive, up-to-date, and provides a seamless way for users to provide feedback.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:in_app_review/in_app_review.dart';
 import 'package:moshaf_app/firebase_options.dart';
 import 'package:moshaf_app/screens/Home/home_page.dart';
 import 'package:moshaf_app/shared/Colors/color_manager.dart';
@@ -11,6 +15,9 @@ import 'package:moshaf_app/shared/Network/dio_helper.dart';
 import 'package:moshaf_app/shared/bloc_observer.dart';
 import 'package:moshaf_app/shared/components/app_font.dart';
 import 'package:moshaf_app/shared/cubit/cubit/internet_bloc.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:upgrader/upgrader.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,7 +28,13 @@ void main() async {
   // Initialize Dio helper and Bloc observer
   DioHelper.init();
   Bloc.observer = MyBlocObserver();
-
+  await ScreenUtil.ensureScreenSize();
+  if (kReleaseMode) {
+    await requestReview();
+  }
+  if (kDebugMode) {
+    await Upgrader.clearSavedSettings();
+  }
   runApp(const QuranKareemApp());
 }
 
@@ -30,13 +43,18 @@ Future<void> _initializeFirebase() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // Set up Firebase Crashlytics error handling
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  if (kReleaseMode) {
+    // Pass all uncaught "fatal" errors from the framework to Crashlytics
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+    // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
 }
 
 class QuranKareemApp extends StatelessWidget {
@@ -58,14 +76,26 @@ class QuranKareemApp extends StatelessWidget {
   }
 
   Widget _buildApp() {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QURAN KAREEM',
-      theme: ThemeData(
-        scaffoldBackgroundColor: ColorsManager.kBackgroundColor,
-        fontFamily: AppFonts.fontFamily,
-      ),
-      home: const HomePage(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(
+            textScaler: const TextScaler.linear(1.0),
+          ),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'QURAN KAREEM',
+            theme: ThemeData(
+              scaffoldBackgroundColor: ColorsManager.kBackgroundColor,
+              fontFamily: AppFonts.fontFamily,
+            ),
+            home: const HomePage(),
+          ),
+        );
+      },
     );
   }
 
@@ -74,5 +104,28 @@ class QuranKareemApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
+  }
+}
+
+Future<void> requestReview() async {
+  final InAppReview inAppReview = InAppReview.instance;
+
+  if (await inAppReview.isAvailable()) {
+    await inAppReview.requestReview();
+  } else {
+    goToApplicationOnPlayStore();
+  }
+}
+
+goToApplicationOnPlayStore() async {
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+  String url = '';
+  String packageName = packageInfo.packageName;
+  if (Platform.isAndroid) {
+    url = 'https://play.google.com/store/apps/details?id=$packageName';
+  } else if (!await launchUrl(Uri.parse(url),
+      mode: LaunchMode.externalApplication)) {
+    throw Exception('Could not launch $url');
   }
 }
